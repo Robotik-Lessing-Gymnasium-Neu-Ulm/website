81f1709d574f07a2d2c84203d107e7a53c254d17
carlaTechnulgy <strassburgercarla@technulgy.com>
2024-05-15 23:11:50 +0200

    Gltf File verlinkt

--- themes/spectral/assets/js/main.js
+++ themes/spectral/assets/js/main.js
@@ -28,7 +28,7 @@ const loader = new GLTFLoader();
 
 //Load the file
 loader.load(
-  `models/${objToRender}/scene.gltf`,
+  `models/${objToRender}/Open2023 Bordeaux.gltf`,
   function (gltf) {
     //If the file is loaded, add it to the scene
     object = gltf.scene;
